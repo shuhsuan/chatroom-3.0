6bd6abb89403da57350519df9e677c97f3bf30c9
shuhsuan <shuhsuanchang.9@gmail.com>
2023-07-10 17:12:18 +0100

    welcome page styling

--- src/Components/welcome.js
+++ src/Components/welcome.js
@@ -11,14 +11,15 @@ const Welcome = () => {
   return (
     <main className="welcome">
       <h2>
-        Hello friends! Welcome to the chat :{")"} <br/>
-        Here are the chat rules: <br/><br/>
+        Hello friends! Welcome to the chat :{")"} <br/><br/>
+        </h2>
+        Here are the chat rules: <br/>
           1. Only english is allowed<br/>
           2. Chats will be deleted after 24 hours<br/>
-          {"("}Or it would be if I bought the firebase blaze subscription{")"}
           <br/><br/>
-        This is a safe learning space for everyone!
-      </h2>
+
+        <h2>This is a safe learning space for everyone!</h2>
+      
       <p>Sign in with Google to chat with with your fellow english practitioners :D</p>
       <img src={login} onClick={googleSignIn}>
       </img>
